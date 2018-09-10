283aa12bd9281988b53fc743062fbcd9d710bfff
GeorginaXu <cxu@smith.edu>
2018-09-10 16:50:45 -0400

    Initial Commit

---
+++ Tumblr_Feed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tumblr_Feed
+//
+//  Created by georginaxu on 9/10/18.
+//  Copyright © 2018 georginaxu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
